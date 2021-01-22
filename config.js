cc0c35c1bf4acd33a2d6136746a2b02dfd233618
milton <milton0330@gmail.com>
2021-01-22 20:35:36 -0300

    AGREGAGO DE DOCKERIGNORE

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-exports.KEYWEATHERMAP = //apiKey de Open Weather Map
+exports.KEYWEATHERMAP = '26bb2aaa4bdf79d43d9400a5393800ba'//apiKey de Open Weather Map
 exports.IP_API = "http://ip-api.com/json/"
